689b9de2af5f1fef6d8025d950c1a45688387bb1
TanevskiiAndrei <tanevskiya@gmail.com>
2024-04-23 21:04:19 +0300

    изменение структуры

---
+++ A0_19/HW_A16/HW_A16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+    scanf ("%d%d%d", &a, &b, &c);
+    if(a<b)
+    {
+        if(b<c)
+        {
+             printf("YES");
+        }
+        else
+        {
+             printf("NO");
+        }
+    }
+    else
+    {
+       printf("NO");
+    }
+    return 0;
+}
+
---
+++ A0_19/HW_A6/HW_A6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,dif;
+    scanf ("%d%d", &a, &b);
+    dif = a - b;
+    printf("%d" ,dif);
+    return 0;
+}
+
